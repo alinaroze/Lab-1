4e303f1c04d2a1ce747503c28386b7c0a3cb4fc1
Alina Rozenbaum <alinaroze94@gmail.com>
2016-02-09 11:41:30 -0800

    Full Upload

---
+++ src/Lab1b.java
@@ -0,0 +1,66 @@
+
+/**
+ * CS 2003 - Lab 1. Code to compute the sum of doubles from an input
+ * file.  NOTE: add data members and methods at your convenience. Use
+ * the most accurate data types. You can reuse bits of code from
+ * the class Lab1a.
+ *
+ * @author Alina Rozenbaum
+ * 
+ */
+
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+public class Lab1b {
+    
+    // data members
+
+    /** Constructor: calls method readFile that reads the doubles
+     * contained in the file "<code>filename</code>" and computes the
+     * sum of the elements.
+     @param filename name of the file containing doubles.
+    */
+    public Lab1b(String filename) {
+	readFile(filename);
+    }
+
+    /** Reads double from a file named <code>filename</code> and
+     * computes the sum of the elements contained in the file 
+     * @param filename name of the file containing the doubles 
+     */
+    public void readFile(String filename) {
+    	try {
+    	File inputFile = new File(filename);
+    	Scanner input = new Scanner(inputFile);
+    	double sum=0, elt;
+    			    
+    	// store all elements in a vector
+    			   
+    	while (input.hasNextDouble()) {
+    	elt = input.nextDouble();
+    	sum = sum + elt;
+    	}
+    	System.out.println("The summation of the numbers in the input of file: \n"+sum);
+    			    
+    	    	
+    	} 
+    	catch (IOException e) {
+    	System.err.println("IOException in reading input file!!!");
+    	}	
+    	} //end readFile(
+    
+    
+
+    /** main : creates a Lab1b Object with the filename passed in
+     * argument*/
+    public static void main(String args[]) {
+	if (args.length == 0)
+	    System.err.println("enter the data file name!");
+	else
+	    new Lab1b(args[0]);
+    } //end main
+    
+} //end class Lab1b
